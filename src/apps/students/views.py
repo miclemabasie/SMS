2be7a28c22f929cbf309a6414435b649def08d8f
miclemabasie <miclemabasie3@gmail.com>
2024-03-10 13:26:20 +0100

    student detail view done

--- src/apps/students/views.py
+++ src/apps/students/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import StudentProfile
+from django.contrib.auth.models import User
 
 
 def list_student_view(request):
@@ -9,6 +10,17 @@ def list_student_view(request):
     template_name = "students/list.html"
     context = {
         "students": queryset,
+    }
+
+    return render(request, template_name, context)
+
+
+def student_detail_view(request, matricule, pkid):
+    student = get_object_or_404(StudentProfile, matricule=matricule, pkid=pkid)
+
+    template_name = "students/details.html"
+    context = {
+        "student": student
     }
 
     return render(request, template_name, context)
