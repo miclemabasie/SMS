7c405d315134eb5525f5552ad6e418f9460dbf11
miclemabasie <miclemabasie3@gmail.com>
2024-07-29 10:16:05 +0100

    updated allowed hosts

--- src/sms/settings/development.py
+++ src/sms/settings/development.py
@@ -9,7 +9,6 @@ INSTALLED_APPS += DEVELOPMENT_APPS
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
