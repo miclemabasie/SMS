2e094b87a11a4c97ab61e62e3673556e3b6c110c
miclemabasie <miclemabasie3@gmail.com>
2024-07-29 10:20:56 +0100

    updated session template tag

--- src/apps/terms/templatetags/session_tags.py
+++ src/apps/terms/templatetags/session_tags.py
@@ -8,10 +8,14 @@ from apps.terms.models import AcademicYear, ExaminationSession, Term
 
 @register.simple_tag
 def current_working_session():
-    current_year = AcademicYear.objects.get(is_current=True)
-    current_terms = Term.objects.filter(is_current=True, academic_year=current_year)
-    if current_terms.exists():
-        current_term = current_terms.first()
-        info = f"current session: {current_year.name}-{current_term.term}"
-        return info
-    return "Invalid session-term configuration."
+    current_year = AcademicYear.objects.filter(is_current=True)
+    if current_year.exists():
+        current_year = current_year.first()
+        current_terms = Term.objects.filter(is_current=True, academic_year=current_year)
+        if current_terms.exists():
+            current_term = current_terms.first()
+            info = f"current session: {current_year.name}-{current_term.term}"
+            return info
+        return None
+    else:
+        return None
